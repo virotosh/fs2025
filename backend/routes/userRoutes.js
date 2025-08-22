e475726f222227f424e24ba89f8ecc4a15e37b56
viro <vuonghiit@gmail.com>
2025-08-22 21:29:44 +0300

    userprofile with authentication

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -8,8 +8,13 @@ router.get('/', (req, res) => {
 const {
 	registerUser,
     loginUser,
+    userProfile,
 } = require("../controllers/userController");
+
+const authMiddleware = require("../middleware/authMiddleware");
+
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.post("/profile", authMiddleware, userProfile);
 
 module.exports = router;
