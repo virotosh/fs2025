4b7c7fa65ca29c50040e317a01780b4c7439dd29
viro <vuonghiit@gmail.com>
2025-08-24 18:50:47 +0300

    home page connect items api

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,14 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(),react()],
+  server: {
+		proxy: {
+			"/api": {
+				target: "http://localhost:5000/api",
+				changeOrigin: true,
+				secure: true,
+				rewrite: (path) => path.replace(/^\/api/, ""),
+			},
+		},
+	},
 })
