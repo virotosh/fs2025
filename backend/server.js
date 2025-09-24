49a6a46439f7ae2441b14cd741e6a4659285cdf0
viro <vuonghiit@gmail.com>
2025-09-24 12:57:10 +0300

    connect mongodb atlas

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,28 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
-
-const router = express.Router();
-
+const mongoose = require("mongoose");
 
 dotenv.config();
 
+// connect mongodb atlas
+const connectDB = async () => {
+    try {
+       mongoose.set('strictQuery', false);
+       await mongoose.connect(process.env.MONGODB_URI, {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+       });
+       console.log("MongoDB Connected");
+    } catch (error) {
+       console.error("Error connecting to MongoDB:", error);
+       process.exit(1);
+    }
+}
+
+connectDB();
+
+const router = express.Router();
 
 const app = express();
 
@@ -27,5 +43,3 @@ app.use(router);
 app.use("/api/users", require("./routes/userRoutes"));
 
 app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
-
-mongodb+srv://tungvuong:MQTs0ki6SMqtI4xh@cluster0.qli2kmd.mongodb.net/
